8850a90a5a86dac17970b2b34a722b701af1a1c3
Roushan022 <roushan.akhtar_bca25@theaims.ac.in>
2026-08-05 23:43:31 +0530

    Create 06_unit_price_comp.py

---
+++ 06_unit_price_comp.py
@@ -0,0 +1,8 @@
+item_a=(4.50,6)
+item_b=(7.20,10)
+print("Item A",item_a)
+print("Iitem B",item_b)
+if (item_a[0]/item_b[1])>(item_b[0]/item_b[1]):
+    print("Item A is cheaper per unit")
+else:
+    print("Item B is cheaper per unit ")
