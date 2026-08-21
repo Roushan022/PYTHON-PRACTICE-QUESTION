4d6afa706b19901439c74142ae0cb22e97420c47
Roushan Akhtar <roushan.akhtar_bca25@theaims.ac.in>
2026-08-21 21:21:09 +0530

    Implement recursive GCD function
    Added a recursive method to calculate GCD.

--- 28_GCD.py
+++ 28_GCD.py
@@ -2,3 +2,15 @@ import math as m
 a=int(input("Enter 1st number:- "))
 b=int(input("Enter 2nd number:- "))
 print("GCD of the numebr of ",m.gcd(a,b))
+
+
+Recusrion Method
+def gcd(a,b):
+    if b==0:
+        return a
+    else:
+        return gcd(b,a%b)
+a=int(input("Enter  a num:- "))
+b=int(input("Enter another number:- "))
+GCD=gcd(a,b)
+print(GCD)
