093abbbdf3e319dd9659ba2131739d3947861be6
Roushan Akhtar <roushan.akhtar_bca25@theaims.ac.in>
2026-09-01 23:50:36 +0530

    Create 45_TUPLE_.py

---
+++ 45_TUPLE_.py
@@ -0,0 +1,11 @@
+
+numbers = (10, 20, 30, 40, 50)
+print("First number:", numbers[0])
+print("Third number:", numbers[2])
+
+print("Length:", len(numbers))
+for num in numbers:
+    print(num)
+print("Sum:", sum(numbers))
+print("Max:", max(numbers))
+print("Min:", min(numbers))
